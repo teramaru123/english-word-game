73b63aab82653366d538c3bceea77e8b1210f225
i298naka <138636696+i298naka@users.noreply.github.com>
2023-11-22 20:40:53 +0900

    Update main_Image.js

--- main_Image.js
+++ main_Image.js
@@ -1,10 +1,11 @@
 // - global -------------------------------------------------------------------
 var screenCanvas, info;
 var run = true;
-var fps = 1000 / 30;
 var mouse = new Point();
-var ctx; //コンテキスト格納
-var own = new Image(); //自機画像
+var ctx; // コンテキスト格納
+var own = new Image(); // 自機画像
+var bulletImage = new Image(); // 弾画像
+var bulletList = [];
 
 // - main ---------------------------------------------------------------------
 window.onload = function(){
@@ -15,22 +16,25 @@ window.onload = function(){
   screenCanvas.width = 256;
   screenCanvas.height = 256;
 
-  //2dコンテキスト
+  // 2dコンテキスト
   ctx = screenCanvas.getContext('2d');
 
   // イベントの登録
   screenCanvas.addEventListener('mousemove', mouseMove, true);
   window.addEventListener('keydown', keyDown, true);
+  screenCanvas.addEventListener('mousedown', fireBullet, true);
 
   // エレメント関連
   info = document.getElementById('info');
 
-  //自機初期化
+  // 画像初期化
   own.src = "own.png";
+  bulletImage.src = "bullet.png";
 
+  gameloop();
 
   // ループ処理を呼び出す
-  (function(){
+  function gameloop(){
     // HTMLを更新
     info.innerHTML = mouse.x + ' : ' + mouse.y;
 
@@ -38,12 +42,23 @@ window.onload = function(){
     ctx.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
 
     // 自機の画像を表示
-    ctx.drawImage(own,mouse.x-10,mouse.y-10);
+    ctx.drawImage(own, mouse.x-10, mouse.y-10);
 
+    // 弾の描画と移動
+    for (var i = bulletList.length-1; i >= 0 ; i--) {
+      bulletList[i].move();
+
+      // 弾が画面外に出たら削除
+      if (bulletList[i].y < 0) {
+        bulletList.splice(i, 1);
+      } else {
+        bulletList[i].draw();
+      }
+    }
 
     // フラグにより再帰呼び出し
-    if(run){setTimeout(arguments.callee, fps);}
-  })();
+    if(run){requestAnimationFrame(gameloop);}
+  }
 };
 
 // - event --------------------------------------------------------------------
@@ -52,7 +67,6 @@ function mouseMove(event){
   mouse.x = event.clientX - screenCanvas.offsetLeft;
   mouse.y = event.clientY - screenCanvas.offsetTop;
 }
-
 
 function keyDown(event){
   // キーコードを取得
