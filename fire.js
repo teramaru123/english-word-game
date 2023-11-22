14030ce26a4f1ab28d077faaebcc7ac4144500d1
i298naka <138636696+i298naka@users.noreply.github.com>
2023-11-22 20:39:49 +0900

    Update fire.js

--- fire.js
+++ fire.js
@@ -1,13 +1,11 @@
-var Bullet = function(x, y) {
+// 弾のクラス定義
+function Bullet(x, y) {
   this.x = x;
   this.y = y;
   this.speed = 5;
 
   this.move = function() {
     this.y -= this.speed;
-    if (this.y < 0) {
-      bulletList.splice(bulletList.indexOf(this), 1);
-    }
   }
 
   this.draw = function() {
@@ -15,7 +13,8 @@ var Bullet = function(x, y) {
   }
 }
 
-function mouseClick(event) {
+// 弾を発射する関数
+function fireBullet() {
   var bullet = new Bullet(mouse.x, mouse.y);
   bulletList.push(bullet);
 }
