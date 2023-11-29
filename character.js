fab67c1cf26d0cfc910771f1e0f65ead409e76fc
i298naka <138636696+i298naka@users.noreply.github.com>
2023-11-29 20:34:38 +0900

    Update character.js

--- character.js
+++ character.js
@@ -1,8 +1,27 @@
-function Character(){
-  this.position = new Point();
-   this.size = 0;
-}
+// Playerクラスの定義
+function Player() {
+  this.x = 128;
+  this.y = 128;
+  this.speed = 5; // プレイヤーの移動速度
+  this.moveUp = false;
+  this.moveLeft = false;
+  this.moveDown = false;
+  this.moveRight = false;
 
-Character.prototype.init = function(size){
-  this.size = size;
-};
+  // プレイヤーの更新処理
+  this.update = function() {
+    // 上下左右の移動
+    if (this.moveUp) {
+      this.y -= this.speed;
+    }
+    if (this.moveLeft) {
+      this.x -= this.speed;
+    }
+    if (this.moveDown) {
+      this.y += this.speed;
+    }
+    if (this.moveRight) {
+      this.x += this.speed;
+    }
+  };
+}
