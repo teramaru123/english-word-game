9ef824cd08afdbbba87e69823a32dbc7e575b784
i468yano <138637077+i468yano@users.noreply.github.com>
2023-11-30 20:15:04 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>可変なCanvasサイズ</title>
+    <title>ホーム画面</title>
     <style>
       canvas {
         display: block;
