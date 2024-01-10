d7df29b4c6363bbadc921a573ab96e9a0927e836
i298naka <138636696+i298naka@users.noreply.github.com>
2024-01-10 20:17:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -10,12 +10,13 @@ var bulletImage = new Image(); // 弾画像
 var bulletList = [];
 var enemyBulletList = [];
 var shotSound = new Audio('shot.mp3'); // "shot.mp3" ファイルを読み込む
+var lastKeyDownTime = 0;
+var ans = 0; //プレイヤーの回答を格納
 
-// ScoreManager クラス
 class ScoreManager {
   constructor() {
     this.score = 0;
-    this.scoreInterval = null; // スコア増加の setInterval を管理するための変数を追加
+    this.scoreInterval = null;
   }
 
   startScoring() {
@@ -26,14 +27,14 @@ class ScoreManager {
   }
 
   stopScoring() {
-    clearInterval(this.scoreInterval); // スコア増加の setInterval をクリア
+    clearInterval(this.scoreInterval);
   }
 
   displayScore() {
     info.innerHTML = "現在のスコア: " + this.score;
   }
 
-  getCurrentScore(){
+  getCurrentScore() {
     return this.score;
   }
 }
@@ -61,24 +62,72 @@ window.onload = function(){
   enemyImage.src = "enemy.png";
   bulletImage.src = "bullet.png";
 
-  const scoreManager = new ScoreManager();
+  var scoreManager = new ScoreManager();
   scoreManager.startScoring();
   // keymove.js で参照できるように scoreManager をグローバルに設定
   window.scoreManager = scoreManager;
+
 
   gameloop();
 
   // ループ処理を呼び出す
   function gameloop(){
     // HTMLを更新
-    info.innerHTML = "現在のスコア: " + scoreManager.score;
+    info.innerHTML = "現在のスコア: " + scoreManager.score + "<br>位置: " + ans;
 
     // screenクリア
     ctx.clearRect(0, 0, screenCanvas.width, screenCanvas.height);
 
+    // 背景を半透明に
+    ctx.fillStyle = "rgba(255, 0, 0, 0.2)";
+    ctx.fillRect(0, 0, screenCanvas.width / 2, screenCanvas.height / 2);
+
+    ctx.fillStyle = "rgba(0, 0, 255, 0.2)";
+    ctx.fillRect(screenCanvas.width / 2, 0, screenCanvas.width / 2, screenCanvas.height / 2);
+
+    ctx.fillStyle = "rgba(255, 255, 0, 0.2)";
+    ctx.fillRect(0, screenCanvas.height / 2, screenCanvas.width / 2, screenCanvas.height / 2);
+
+    ctx.fillStyle = "rgba(0, 128, 0, 0.2)";
+    ctx.fillRect(screenCanvas.width / 2, screenCanvas.height / 2, screenCanvas.width / 2, screenCanvas.height / 2);
+
     // 自機、敵機の画像を表示
     ctx.drawImage(own, player.x-10, player.y-10);
     ctx.drawImage(enemyImage, enemy.x-10, enemy.y-10);
+
+    for (var i = enemyBulletList.length-1; i >= 0 ; i--) {
+
+      if (enemyBulletList[i].y > screenCanvas.height) {
+        enemyBulletList.splice(i, 1);
+      } else {
+        enemyBulletList[i].draw();
+      }
+
+      if (!enemyBulletList[i]) {
+        continue; // 要素が存在しない場合はスキップ
+      }
+
+      if (
+        enemyBulletList[i].x > player.x - 10 &&
+        enemyBulletList[i].x < player.x + 10 &&
+        enemyBulletList[i].y > player.y - 10 &&
+        enemyBulletList[i].y < player.y + 10 &&
+        Date.now() - lastKeyDownTime > 3000
+      ) {
+        // 当たった場合の処理
+        run = false;
+        scoreManager.stopScoring(); // スコア増加の setInterval を停止
+        alert('ゲームオーバー\nスコア：'+ scoreManager.getCurrentScore());
+
+        // アラートを閉じてから別のページに遷移
+        setTimeout(function () {
+          window.location.href = './result.html';  // 遷移先のURLを指定
+        }, 10);
+        return;
+      }
+      enemyBulletList[i].move();
+
+    }
 
     // 弾の描画と移動
     for (var i = bulletList.length-1; i >= 0 ; i--) {
@@ -92,34 +141,7 @@ window.onload = function(){
       }
     }
 
-    for (var i = enemyBulletList.length-1; i >= 0 ; i--) {
-      enemyBulletList[i].move();
 
-      if (enemyBulletList[i].y > screenCanvas.height) {
-        enemyBulletList.splice(i, 1);
-      } else {
-        enemyBulletList[i].draw();
-      }
-
-      if (
-        enemyBulletList[i].x > player.x - 10 &&
-        enemyBulletList[i].x < player.x + 10 &&
-        enemyBulletList[i].y > player.y - 10 &&
-        enemyBulletList[i].y < player.y + 10
-      ) {
-        // 当たった場合の処理
-        run = false;
-        scoreManager.stopScoring(); // スコア増加の setInterval を停止
-        alert('ゲームオーバー\nスコア：'+ scoreManager.getCurrentScore());
-
-        // アラートを閉じてから別のページに遷移
-        setTimeout(function () {
-          window.location.href = './underconstruction.html';  // 遷移先のURLを指定
-        }, 10);
-        return;
-      }
-
-    }
 
     // プレイヤー、敵機の移動処理
     player.update();
@@ -129,10 +151,24 @@ window.onload = function(){
     player.x = Math.max(10, Math.min(player.x, screenCanvas.width - 10));
     player.y = Math.max(10, Math.min(player.y, screenCanvas.height - 10));
 
+    // 自機の位置に応じてanswer変数に値を格納
+    if (player.x <= screenCanvas.width / 2 && player.y <= screenCanvas.height / 2) {
+      // 左上
+      ans = 1;
+    } else if (player.x > screenCanvas.width / 2 && player.y <= screenCanvas.height / 2) {
+      // 右上
+      ans = 2;
+    } else if (player.x > screenCanvas.width / 2 && player.y > screenCanvas.height / 2) {
+      // 右下
+      ans = 3;
+    } else if (player.x <= screenCanvas.width / 2 && player.y > screenCanvas.height / 2) {
+      // 左下
+      ans = 4;
+    }
+
 
     // フラグにより再帰呼び出し
     if(run){requestAnimationFrame(gameloop);}
   }
 
 };
-
