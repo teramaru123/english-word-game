e2b125c7987fdb2e9bb9d98505c212a0b6979aa6
i298naka <138636696+i298naka@users.noreply.github.com>
2024-01-24 11:33:10 +0900

    Update enemy.js

--- enemy.js
+++ enemy.js
@@ -1,16 +1,31 @@
 function Enemy() {
   this.x = 50;
   this.y = 50;
-  this.speed = 2;
+  this.nextx = 0;
+  this.nexty = 0;
+  this.speed = 0.1;
+
+  function GenerateRandomPosition(){
+    this.nextx = Math.random() * 128;
+    this.nexty = Math.random() * 128;
+  }
+
+  GenerateRandomPosition.call(this);  // 初回のランダムな位置を生成
 
   this.update = function() {
-    this.x += this.speed;
+    var dx = this.nextx - this.x;
+        var dy = this.nexty - this.y;
+        var distance = Math.sqrt(dx * dx + dy * dy);
 
-    if (this.x > screenCanvas.width) {
-      this.x = 0;
-    }
+        if (distance > this.speed) {
+            this.x += (dx / distance) * this.speed;
+            this.y += (dy / distance) * this.speed;
+        } else {
+            // 目標に到達したら新しいランダムな位置に移動
+            GenerateRandomPosition.call(this);
+        }
 
-    if (Math.random() < 0.02) {
+    if (Math.random() < 0.15) {
       var enemyBullet = new EnemyBullet(this.x, this.y);
       enemyBulletList.push(enemyBullet);
     }
@@ -22,12 +37,18 @@ function EnemyBullet(x, y) {
   this.y = y;
 
   this.speed = 1;
+  this.angle = Math.random() * Math.PI * 2; //ランダムな方向性
 
   this.move = function() {
-    this.y += this.speed;
+    this.x += this.speed * Math.cos(this.angle);
+    this.y += this.speed * Math.sin(this.angle);
   };
 
   this.draw = function() {
-    ctx.drawImage(bulletImage, this.x, this.y,10,10);
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle + Math.PI / 2);
+    ctx.drawImage(bulletImage,-5, -5, 10, 10);
+    ctx.restore();
   };
 }
